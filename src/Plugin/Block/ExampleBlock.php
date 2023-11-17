48d925d6d59fec1c9858908fae7f43cc70db1ee6
norsargsyan <norayrsargsyan1@gmail.com>
2023-11-17 16:03:02 +0400

    Finishing creating configuration form

--- src/Plugin/Block/ExampleBlock.php
+++ src/Plugin/Block/ExampleBlock.php
@@ -35,7 +35,6 @@ class ExampleBlock extends BlockBase {
   }
 
   /**
-
    * {@inheritdoc}
    */
   public function getCacheTags() {
@@ -52,7 +51,7 @@ class ExampleBlock extends BlockBase {
   public function defaultConfiguration()
   {
     return [
-      'hello_block_name' => $this->t(''),
+      'block_custom_setting' => $this->t(''),
     ];
   }
 
@@ -61,16 +60,33 @@ class ExampleBlock extends BlockBase {
    */
   public function blockForm($form, FormStateInterface $form_state)
   {
-    $form['hello_block_name'] = [
+    $form['block_custom_setting'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Who'),
-      '#description' => $this->t('Who do you want to say hello to?'),
-      '#default_value' => $this->configuration['hello_block_name'],
+      '#description' => $this->t('Enter a custom setting for the block.'),
+      '#default_value' => $this->configuration['block_custom_setting'],
     ];
 
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->configuration['block_custom_setting'] = $values['block_custom_setting'];
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate( $form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('block_custom_setting');
+
+    // Perform your custom validation logic here.
+    if (strlen($value) < 10) {
+      $form_state->setError($form, $this->t('Custom Setting must be at least 10 characters long.'));
+    }
+  }
 
 }
